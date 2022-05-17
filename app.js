5ef4d619be62248ac6a80eece1d0168cd0e540f7
Bibhuni <bsahoo1024@gmail.com>
2022-05-17 14:27:58 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Declare elements:
+const bmiForm = document.getElementById("bmi-form");
+const bmiSubmit = document.getElementById("bmi-submit");
+const calorieForm = document.getElementById("calorie-form");
+const calorieSubmit = document.getElementById("calorie-submit");
+const showExercise = document.getElementById("show-exercise");
+const exerciseSelect = document.getElementById("exercise-select");
+const exercisePic = document.getElementById("exercise-pic");
+// *****************
